611160880faa7c9e0dd2c7edf59896b56020dd1e
NetSage <alexsmith@purelymail.com>
2025-11-02 08:06:34 -0600

    Add the player sprite and made it so you can move around.

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import pygame
 from constants import *
+from player import Player
+from circleshape import CircleShape
+
+updateable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updateable, drawable)
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    dt = clock.tick(60) / 1000
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updateable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         clock.tick(60)
     print("Starting Asteroids!")
