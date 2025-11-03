95b7c3cc829b7f3edd601cf807e7882f7520d23d
NetSage <alexsmith@purelymail.com>
2025-11-03 07:00:14 -0600

    Got "asteroids" to spawn and move in random directions.

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import pygame
+from asteroidfield import *
 from constants import *
 from player import Player
 from circleshape import CircleShape
 
-updateable = pygame.sprite.Group()
-drawable = pygame.sprite.Group()
-Player.containers = (updateable, drawable)
-
 
 def main():
     pygame.init()
+
+    asteroids = pygame.sprite.Group()
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable,)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     dt = clock.tick(60) / 1000
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
