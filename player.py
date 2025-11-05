f15506fc735b001dc1e2a4782f04232638cd8790
NetSage <alexsmith@purelymail.com>
2025-11-05 06:36:23 -0600

    Limited the fire rate to a more reasonable level

--- player.py
+++ player.py
@@ -1,13 +1,20 @@
 import pygame
 from shot import Shot
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_SHOT_SPEED, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import (
+    PLAYER_RADIUS,
+    PLAYER_SHOOT_COOLDOWN,
+    PLAYER_SHOT_SPEED,
+    PLAYER_TURN_SPEED,
+    PLAYER_SPEED,
+)
 
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_timer = 0
 
     # in the player class
     def triangle(self):
@@ -26,6 +33,8 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def update(self, dt):
+        self.shot_timer -= dt
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -46,5 +55,7 @@ class Player(CircleShape):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def shoot(self):
-        velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
-        Shot(self.position.x, self.position.y, velocity)
+        if self.shot_timer <= 0:
+            velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
+            Shot(self.position.x, self.position.y, velocity)
+            self.shot_timer = PLAYER_SHOOT_COOLDOWN
