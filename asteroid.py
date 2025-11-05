f582fbaeb5cacb176281ebb3f705566849224b67
NetSage <alexsmith@purelymail.com>
2025-11-05 06:59:28 -0600

    Larger asteroids now split when hit spawning 2 smaller asteroids.

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
 
 
 class Asteroid(CircleShape):
@@ -11,3 +13,18 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            self.kill()
+        else:
+            angle_change = random.uniform(20, 50)
+            velocity1 = self.velocity.rotate(angle_change)
+            velocity2 = self.velocity.rotate(-angle_change)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            new_asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+            new_asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+            new_asteroid1.velocity = velocity1
+            new_asteroid2.velocity = velocity2
+            self.kill()
+            return new_asteroid1, new_asteroid2
--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
                 sys.exit()
             for shot in shots:
                 if shot.collision(asteroid):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         pygame.display.flip()
